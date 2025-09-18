2e0cdea570a9f79927f53acdbf301215cc6cfdd4
SYERA AL-FARIDZI <syeraalfaridzi03@gmail.com>
2025-09-18 20:41:28 +0800

    Dev: (#2)
    * feat: implemented user login
    
    * feat: update authentication to use Google sign-in and refactor login form

---
+++ components/auth/sigin-button.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+
+import { signIn } from "next-auth/react"
+
+export function SignIn() {
+    return <Button onClick={() => signIn()}>Sign In</Button>
+}
