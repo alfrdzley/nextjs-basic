8617ac0b126d5bbacba6e6311e6980ed6623728e
SYERA AL-FARIDZI <syeraalfaridzi03@gmail.com>
2025-09-28 21:29:31 +0800

    feat: remove SessionProvider for better-auth integration

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -12,20 +12,36 @@ import {
 import {Input} from "@/components/ui/input"
 import {Label} from "@/components/ui/label"
 import React from "react";
-import { signIn, useSession } from "next-auth/react"
+import {authClient} from "@/lib/auth-client";
+import {useForm, Controller} from "react-hook-form";
+import {useRouter} from "next/navigation";
 
+type LoginForm = {
+  email: string;
+  password: string;
+}
 
 export function LoginForm({className, ...props}: React.ComponentProps<"div">) {
+  const router = useRouter();
+  const {control, handleSubmit} = useForm<LoginForm>({
+    defaultValues: {
+      email: "admin@admin.com",
+      password: "samarindahijau",
+    }
+  })
 
-  // Hooks
-  const {data: session} = useSession();
+  const onSubmit = async (value: LoginForm) => {
+    const { data, error } = await authClient.signIn.email({
+      email: value.email,
+      password: value.password,
+      rememberMe: true,
+      // callbackURL: "http://localhost:3000/dashboard",
+    });
+    console.log(data)
 
-  if (session) {
-    console.log(session.user);
 
-  } else {
-    console.log('not logged in');
   }
+
 
   return (
       <div className={cn("flex flex-col gap-6", className)} {...props}>
@@ -37,22 +53,35 @@ export function LoginForm({className, ...props}: React.ComponentProps<"div">) {
             </CardDescription>
           </CardHeader>
           <CardContent>
-            <form onSubmit={(e) => e.preventDefault()}>
+            <form onSubmit={handleSubmit(onSubmit)}>
               <div className="grid gap-6">
                 <div className="grid gap-6">
                   <div className="grid gap-3">
                     <Label htmlFor="email">Email</Label>
-                    <Input
-                        id="email"
-                        type="email"
-                        placeholder="user@example.com"
+                    <Controller
+                        control={control}
+                        name='email'
+                        render={({field}) => (
+                            <Input
+                                {...field}
+                                id="email"
+                                type="email"
+                                placeholder="user@example.com"
+                            />
+                        )}
                     />
                   </div>
                   <div className="grid gap-3">
                     <div className="flex items-center">
                       <Label htmlFor="password">Password</Label>
                     </div>
-                    <Input id="password" type="password"/>
+                    <Controller
+                    control={control}
+                    name='password'
+                    render={({field}) => (
+                        <Input {...field} id="password" type="password"/>
+                    )}
+                    />
                     <Button type="submit" className="w-full">
                       Login
                     </Button>
@@ -64,7 +93,7 @@ export function LoginForm({className, ...props}: React.ComponentProps<"div">) {
                     </div>
                   </div>
                   <div className="flex flex-col gap-4">
-                    <Button variant="outline" className="w-full" onClick={() => signIn('google')}>
+                    <Button variant="outline" className="w-full">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path
                             d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
